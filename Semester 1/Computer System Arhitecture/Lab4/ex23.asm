7068fba053636c49cb9f8c3e264b71c569ff50df
Lung Andreea Selena <153758258+lung-andreea-selena@users.noreply.github.com>
2024-02-27 03:35:34 +0200

    Add files via upload

---
+++ Semester 1/Computer System Arhitecture/Lab4/ex23.asm
@@ -0,0 +1,58 @@
+;Given the byte A and the word B, compute the doubleword C as follows:
+;the bits 24-31 of C are the same as the bits of A
+;the bits 16-23 of C are the invert of the bits of the lowest byte of B
+;the bits 10-15 of C have the value 1
+;the bits 2-9 of C are the same as the bits of the highest byte of B
+;the bits 0-1 both contain the value of the sign bit of A
+
+bits 32 ; assembling for the 32 bits architecture
+
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start        
+
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+; our data is declared here (the variables needed by our program)
+segment data use32 class=data
+    a db 10011000b
+    b dw 1001011001010100b
+    c resd 1
+
+; our code starts here
+segment code use32 class=code
+    start:
+    ;the bits 24-31 of C are the same as the bits of A
+    mov eax,0
+    mov al,[a]
+    mov cl,8
+    shl eax,cl;ah is ocupied with A's bits
+    ;the bits 16-23 of C are the invert of the bits of the lowest byte of B
+    mov bx,[b]
+    not bx
+    or al,bl;in al are the invert 0-7 bits of B
+    mov cl,16
+    shl eax,cl;first 16 bits of eax are ocupied
+    ;the bits 10-15 of C have the value 1
+    or ax, 1111110000000000b;first 22 are ocupied
+    ;the bits 2-9 of C are the same as the bits of the highest byte of B
+    mov dx,0
+    mov bx,[b]
+    or dl,bh
+    mov cl,2
+    shl dx,cl;first bits 2-9 of dx are ocupied
+    ;the bits 0-1 both contain the value of the sign bit of A
+    mov bl,[a]
+    mov cl,1
+    sar bl,cl;bits 6-7 of bl we have sign bit of A
+    mov cl,2
+    rol bl,cl;first 2 bits of bl are sign bit of A
+    and bl, 00000011b
+    or dl,bl;in dx we have what we need
+    or ax,dx
+    mov [c],eax
+    
+    push    dword 0      ; push the parameter for exit onto the stack
+    call    [exit]       ; call exit to terminate the program
---
+++ Semester 1/Computer System Arhitecture/Lab4/ex8.asm
@@ -0,0 +1,75 @@
+;Given the words A and B, compute the byte C as follows:
+;the bits 0-5 are the same as the bits 5-10 of A
+;the bits 6-7 are the same as the bits 1-2 of B.
+;Compute the doubleword D as follows:
+;the bits 8-15 are the same as the bits of C
+;the bits 0-7 are the same as the bits 8-15 of B
+;the bits 24-31 are the same as the bits 0-7 of A
+;the bits 16-23 are the same as the bits 8-15 of A.
+
+bits 32 
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start        
+
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+; our data is declared here (the variables needed by our program)
+segment data use32 class=data
+    
+    a dw 1001110011010110b
+    b dw 1010111000110101b
+    c resb 1
+    d resd 1
+
+; our code starts here
+segment code use32 class=code
+    start:
+    ;the bits 0-5 are the same as the bits 5-10 of A
+    mov ax,[a]
+    and ax, 0000011111100000b
+    mov cl,5
+    shr ax,cl;in al we have first 6 bits same as 5-10 A => c=00111111 (1 ocupied bits)
+    or [c],al; c=00111111
+    ;the bits 6-7 are the same as the bits 1-2 of B.
+    mov ax,[b]
+    and ax,0000000000000110b
+    mov cl,5
+    shl ax,cl; => c=11111111
+    or [c],al
+    ;Compute the doubleword D as follows:
+    ;the bits 24-31 are the same as the bits 0-7 of A
+    mov eax,0
+    mov ax,[a]
+    and ax,0000000011111111b
+    mov cl,8
+    shl eax,cl;the bits of A are on ax and then we work al and then shift all to the left and so on
+    ;the bits 16-23 are the same as the bits 8-15 of A
+    mov bx,[a]
+    and bx,1111111100000000b
+    mov cl,8
+    shr bx,cl;=>0000000011111111
+    or ax,bx;=> 1111111111111111
+    mov cl,8
+    shl eax,cl; first 16 bits are ocupied and the last 16 are empty
+    ;the bits 8-15 are the same as the bits of C
+    mov bl,[c]
+    or al,bl;because we work with all the bits of c and we don't need a mask
+    mov cl,8
+    shl eax,cl; fist 24 bits are ocupied in eax
+    ;the bits 0-7 are the same as the bits 8-15 of B
+    mov bx,[b]
+    and bx,1111111100000000b
+    mov cl,8
+    shr bx,cl
+    or ax,bx;in eax is d
+    mov [d],eax
+    
+    
+    
+    
+    
+    push    dword 0      ; push the parameter for exit onto the stack
+    call    [exit]       ; call exit to terminate the program
